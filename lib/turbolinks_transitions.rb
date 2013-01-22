e2a26f4b59c994bbc757efc5115ec86602ee6593
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-01-21 22:52:53 -0200

    first version

---
+++ lib/turbolinks_transitions.rb
@@ -0,0 +1,8 @@
+require "turbolinks_transitions/version"
+
+module TurbolinksTransitions
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+end
---
+++ lib/turbolinks_transitions/version.rb
@@ -0,0 +1,3 @@
+module TurbolinksTransitions
+  VERSION = "0.0.1"
+end
